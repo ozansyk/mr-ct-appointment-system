48152c5504c97ccb5856880b01b345bdf57e8038
ozan.soyak <ozan.soyak@anadoluefes.com>
2024-10-12 13:16:42 +0300

    first commit for mr-ct-appointment-system-project.

---
+++ src/main/java/com/ozansoyak/mr_ct_appointment_system/MrAndCtScanAppointmentSchedulingAndRecommendationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ozansoyak.mr_ct_appointment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrAndCtScanAppointmentSchedulingAndRecommendationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrAndCtScanAppointmentSchedulingAndRecommendationSystemApplication.class, args);
+	}
+
+}
