52be25a83eb324c5ce794992a16750b5f73fd289
ozan.soyak <ozan.soyak@anadoluefes.com>
2024-12-07 00:02:43 +0300

    schedule added.

--- src/main/java/com/ozansoyak/mr_ct_appointment_system/MrAndCtScanAppointmentSchedulingAndRecommendationSystemApplication.java
+++ src/main/java/com/ozansoyak/mr_ct_appointment_system/MrAndCtScanAppointmentSchedulingAndRecommendationSystemApplication.java
@@ -2,8 +2,10 @@ package com.ozansoyak.mr_ct_appointment_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MrAndCtScanAppointmentSchedulingAndRecommendationSystemApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/ozansoyak/mr_ct_appointment_system/service/ScheduleService.java
@@ -0,0 +1,23 @@
+package com.ozansoyak.mr_ct_appointment_system.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class ScheduleService {
+
+    private final AppointmentService appointmentService;
+
+    public ScheduleService(AppointmentService appointmentService) {
+        this.appointmentService = appointmentService;
+    }
+
+    @Scheduled(cron = "15,30 0/1 * * * ?")
+    public void schedule() {
+        log.info("Scheduling started.");
+        appointmentService.getDeviceAvailability(1L);
+        log.info("Scheduling finished.");
+    }
+}
